6d6eae343727abedef23a1c58bb58ea1464dd25c
Ky0_D1a5 <kyotsuyoi@hotmail.com>
2016-08-25 15:25:38 -0300

    At-01-260816

--- GIPPUpdater/frmGIPPUpdater.cs
+++ GIPPUpdater/frmGIPPUpdater.cs
@@ -26,12 +26,12 @@ namespace frmGIPPUpdater
             this.Focus();
             btnUpdate.Enabled = false;
             try
-            {   // Abrir o arquivo para ler.
+            {   // Abrir o arquivo para ler o caminho do servidor.
                 using (StreamReader sr = new StreamReader(@"C:\Program Files (x86)\GIPP\path"))
                 {
-                    // Ler o arquivo para uma string.
+                    // Ler o conteudo do arquivo para uma string.
                     String line = sr.ReadToEnd();
-
+                    // Define o local do .exe de atualização no diretório correspondente.
                     txtFilesPath.Text = line + @"\GIPP Updater\";
                 }
             }
@@ -39,7 +39,7 @@ namespace frmGIPPUpdater
             {
                 MessageBox.Show("Arquivo não pode ser lido: " + ex.Message, "Erro!", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
-
+            // Define o diretório destino do .exe.
             txtToPath.Text = @"C:\Program Files (x86)\GIPP\";
 
             t = new Thread(ThreadProcess);
@@ -56,7 +56,7 @@ namespace frmGIPPUpdater
         {
             try
             {
-                // Get the subdirectories for the specified directory.
+                // Encontra os subdiretórios do diretório especificado.
                 DirectoryInfo dir = new DirectoryInfo(sourceDirName);
 
                 if (!dir.Exists)
@@ -67,13 +67,13 @@ namespace frmGIPPUpdater
                 }
 
                 DirectoryInfo[] dirs = dir.GetDirectories();
-                // If the destination directory doesn't exist, create it.
+                // Se o diretório destinado não existe, crie-o.
                 if (!Directory.Exists(destDirName))
                 {
                     Directory.CreateDirectory(destDirName);
                 }
 
-                // Get the files in the directory and copy them to the new location.
+                // Encontra os arquivos de um diretório e os copia para o novo local.
                 FileInfo[] files = dir.GetFiles();
 
                 foreach (FileInfo file in files)
@@ -90,7 +90,7 @@ namespace frmGIPPUpdater
                     }
                 }
 
-                // If copying subdirectories, copy them and their contents to new location.
+                // Se estiver copiando diretórios, copia qualquer conteúdo para a nova localização.
                 if (copySubDirs)
                 {
                     foreach (DirectoryInfo subdir in dirs)
@@ -117,24 +117,27 @@ namespace frmGIPPUpdater
                 int i = 5;
                 while (i > 0)
                 {
+                    // Mostra em tempo real a espera para iniciar o processo (esse tempo é necessário para evitar que esse Thread inicie seu serviços antes que a Thread principal seja totalmente criada).
                     this.lblFiles.BeginInvoke((MethodInvoker)delegate () { this.lblFiles.Text = "A atualização inicia em " + i.ToString() + " segundos"; });
                     Thread.Sleep(1000);
                     i -= 1;
                 }
-
+                // Guarda em variáveis os caminhos de origem e destino.
                 string sourcePath = @"" + txtFilesPath.Text;
                 string targetPath = @"" + txtToPath.Text;
-
+                // Copia o .exe de atualização.
                 System.IO.File.Copy(sourcePath + "GIPP.exe", targetPath + "GIPP.exe", true);
 
                 i = 5;
                 while (i > 0)
                 {
+                    // Tempo de espera para fechar todas as Threads e ativar o .exe copiado.
                     this.lblFiles.BeginInvoke((MethodInvoker)delegate () {
                         this.lblFiles.Text = "A atualização terminou, reiniciando GIPP em: " + i.ToString() + " segundos"; });
                     Thread.Sleep(1000);
                     i -= 1;
                 }
+                // Executa o .exe.
                 System.Diagnostics.Process.Start(targetPath + "GIPP.exe");
             }
             catch (Exception ex)
@@ -143,6 +146,7 @@ namespace frmGIPPUpdater
                 this.lblFiles.BeginInvoke((MethodInvoker)delegate () { this.lblFiles.Text = "A atualização falhou!"; });
                 this.btnUpdate.BeginInvoke((MethodInvoker)delegate () { btnUpdate.Enabled = true; });
             }
+            // Desativa a Trhead principal.
             this.lblFiles.BeginInvoke((MethodInvoker)delegate () { this.Close(); });
 
             
